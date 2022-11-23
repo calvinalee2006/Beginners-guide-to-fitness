08c6117d2444e71250d42d0f60f8ca4027af6fc1
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-11-23 06:45:19 -0600

    male.js use for testing to hep make radio buttons

--- src/components/male.js
+++ src/components/male.js
@@ -3,20 +3,68 @@ import {NavLink} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function male() {
-const [age, setAge] = useState();
+    const[formData, setFormData] = useState(
+        {firstName:"", lastName:"", email:"", comments:"", isFriendly: true
+            }
+        )
 
-const handleChange =(e) => {
-    setAge(e.target.value);
-}
+    function handleChange(event){
+        const{name, value, type, checked} = event.target
+       setFormData(prevFormData => {
+        return {
+            ...prevFormData,
+            [name]: type === 'checkbox' ? checked : value 
+        }
+       })
+    }
+
     return (
         <>
         <header>Questions</header>
         <p>How old are you?</p>
+
+        <p>Testing 123</p>
         <form>
-            <input type ="radio" value="young" id="young" onChange={handleChange} name="youngAge" />
-            <label htmlFor="young">18-28</label>
+            <input
+            type="text"
+            placeholder="First Name"
+            onChange={handleChange}
+            name="firstName"
+            value={formData.firstName}
+            />
+
+            <input
+            type="text"
+            placeholder="Last Name"
+            onChange={handleChange} 
+            name="lastName"
+            value={formData.lastName}
+            />
+
+            <input
+            type="text"
+            placeholder="e-mail"
+            onChange={handleChange} 
+            name="e-mail"
+            value={formData.email}
+            />
+
+            <textarea
+            placeholder='comments'
+            onChange={handleChange}
+            name="comments"
+            value={formData.comments} />
+
+            <input 
+            type="checkbox"
+            id="isFriendly" 
+            onChange={handleChange}
+            checked={"isFriendly"}
+            name="isFriendly"
+            />
+            <label htmlFor="isFriendly">Are you friendly?</label>
+            <br />
         </form>
-       
         </>
     )
 }
