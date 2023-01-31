859a051d95fcc6abd46d82e718db59dbc39202e5
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-01-31 07:15:24 -0600

    writing in no Survey

--- src/components/UserPages/noSurvey.js
+++ src/components/UserPages/noSurvey.js
@@ -3,7 +3,19 @@ import React from 'react'
 function NoSurvey() {
   return (
     <>
-     <p>Hello!</p>
+     <p>Like we said before, adopting a healthy lifestyle into your weekly routine is going 
+        to seem difficult, but the best way to make this change is to start with small goals 
+        and a slow start. 
+     </p>
+     <h2>Cardio</h2>
+     <p>From personal experience, cardio exercises is the first type of exercise a person thinks
+        of when wanting to get back into shape. Cardio fitness is anything that will:<br />
+            A. Raises your heart and breathing rate<br />
+            B. Improves the function of your heart, lungs and circulatory system.<br />
+            <p>Trevor Thieme, C.S.C.S and Beachbody's senior manager of fitness and nutrition  {/*Beachbodys link */}  </p> 
+     </p>
+     <p>Cardio does not have to be a marathon run or climbing Mt.Everest, It can be something simple as
+        going outside and playing with children or yard work. Here are some more examples of exercise  </p>
     </>
   )
 }
