b7a96f6bcf2b005cbb0afe3b59fd154bf4f0ab6c
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-02-06 06:43:33 -0600

    started the workout table

---
+++ src/components/UserPages/noSurveyImages/WorkoutJournal.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function WorkoutJournal() {
+  return (
+    <>
+      <div className = 'app-container'>
+        <table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Activity</th>
+                    <th>Time</th>
+                    <th>Additional Notes</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr></tr>
+            </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default WorkoutJournal
