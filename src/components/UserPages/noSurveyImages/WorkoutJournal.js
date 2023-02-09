53105dfcd0649909413beb8dffd079c5e853b5d6
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-02-09 07:13:49 -0600

    figuring out my console warnings before I move on.

--- src/components/UserPages/noSurveyImages/WorkoutJournal.js
+++ src/components/UserPages/noSurveyImages/WorkoutJournal.js
@@ -1,31 +1,47 @@
-import React, {useState} from 'react'
-import data from "./mock-data.json"
-import './noSurvey.css'
+import React, {useState} from 'react';
+import {nanoid} from 'nanoid';
+import data from "./mock-data.json";
+import './noSurvey.css';
 function WorkoutJournal() {
-  
+
   const [workoutData, setWorkoutData] = useState(data);
-  const [addWorkoutData, setAddWorkoutData] = useState({
+  const [addFormData, setAddFormData] = useState({
     Date: "", 
     Activity: "",
     Time: "",
     Notes: ""
-  })
+  });
 
-  // needs more work!!
-  // const handleAddFormChange = (event) =>
-  // event.preventDefault();
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
 
-  // const fieldName = event.target.getAttribute('Date');
-  // const fieldValue = event.target.value;
+    const fieldName = event.target.getAttribute('name');
+    const fieldValue = event.target.value;
 
-  // const newFormData = {...addFormData}
-  // newFormData[fieldName] = fieldValue
+    const newFormData = {...addFormData};
+    newFormData[fieldName] = fieldValue;
 
-  // setAddFormData(newFormData)
+    setAddFormData(newFormData);
+  }; 
+
+  const handleAddFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newEntry = {
+      id: nanoid(),
+      Date: addFormData.Date, 
+      Activity: addFormData.Activity,
+      Time: addFormData.Time,
+      Notes: addFormData.Notes
+    }; 
+
+    const newWorkOutData = [...workoutData, newEntry];
+    setWorkoutData(newWorkOutData);
+  }; 
 
   return (
     <>
-    <h1>My workout Journal</h1>
+      <h1>My workout Journal</h1>
       <div className = 'app-container'>
         <table>
             <thead>
@@ -37,43 +53,45 @@ function WorkoutJournal() {
                 </tr>
             </thead>
             <tbody>
-              {workoutData.map((workout)=> <tr>
-                <td>{workout.Date}</td>
-                <td>{workout.Activity}</td>
-                <td>{workout.Time}</td>
-                <td>{workout.Notes}</td>
-                </tr>)}
+              {workoutData.map((workout) => (
+                <tr key={workout.id}>
+                  <td>{workout.Date}</td>
+                  <td>{workout.Activity}</td>
+                  <td>{workout.Time}</td>
+                  <td>{workout.Notes}</td>
+                </tr>
+              ))}
             </tbody>
         </table>
         <h2>Add your exercise</h2>
-        <form>
+        <form onSubmit={handleAddFormSubmit}>
           <input 
-          type = 'text' 
-          name = "Date" 
-          required = "required"
-          placeholder ="Date"
-          // onChange = {handleAddFormChange}
+            type="text" 
+            name="Date" 
+            required="required"
+            placeholder="Date"
+            onChange={handleAddFormChange}
           />
-           <input 
-          type = 'text' 
-          name = "Activity" 
-          required = "required"
-          placeholder ="Activity"
-          // onChange = {handleAddFormChange}
+          <input 
+            type="text" 
+            name="Activity" 
+            required="required"
+            placeholder="Activity"
+            onChange={handleAddFormChange}
           />
-           <input 
-          type = 'text' 
-          name = "Time" 
-          required = "required"
-          placeholder ="Time"
-          // onChange = {handleAddFormChange}
+          <input 
+            type="text" 
+            name="Time" 
+            required="required"
+            placeholder="Time"
+          onChange = {handleAddFormChange}
           />
            <input 
           type = 'text' 
           name = "Notes" 
           required = "required"
           placeholder ="Additional Notes"
-          // onChange = {handleAddFormChange}
+          onChange = {handleAddFormChange}
           />
           <button type ="submit">Add</button>
         </form>
