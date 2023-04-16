11778462702252c3035ac793ce9f77c892333541
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-04-16 14:56:22 -0500

    finished eating healthy page.

--- src/components/Diet/Diet.js
+++ src/components/Diet/Diet.js
@@ -155,37 +155,21 @@ export default function Diet() {
     
     
     <div className ="healthyContainer">
-    <p>When you first start exercising, eating healthy is going to be one of the BIGGEST
-    concerns you are going to worry about. This topic alone scares so many beginners that
-    they give up their commitment to health and succumb to cravings. The fast pace of daily
-    life does not seem to help with the decisions of a persons eating habits either.</p>
-
-    <p>Life moves fast and it always seems like the day has ended before it even began. 
-    To keep up with the passing of times people tend to eat on the fly or plan to pick up 
-    something from a fast food place on the way to their destination. The fast-food choice 
-    is not the best option, even though it looks to be the fastest.  Whether people want to
-    hear it or not, this option is not healthy due to your food not settling in your stomach, 
-    the way you are consuming your food and what they are consuming. </p>
-
-    <p>Consider your body to be the vehicle of your dreams. You want this vehicle to last a 
-    lifetime and be able to keep up with traffic anytime you hit the road. You realize you are
-    running low on fuel (getting hungry) and you pull up to your local gas station. You only have
-    two options. </p>
-    <ul>
-        <li>Option1. This fuel costs $1.25 but it is 90% H2O and 10% fuel.</li>
-        <li>Option 2. This fuel costs $2.00 but it is 90% fuel and 10%H2O. </li>
-    </ul>
-    <p>If you chose option one you saved some money now, but that fuel you just put in is going to hurt your 
-        vehicle in more ways than one. That fuel will burn quickly, causing you to need more gas and spend more
-        money. Your injectors will get clogged because that fuel is only there to dissolve faster, it is not trying
-        to clean your engine or consider your fuel efficiency. If you chose option two, you might have spent more money,
-        but that fuel cleans your injectors, improves efficiency, and helps your vehicle get from zero to eighty in seven
-        seconds. Best part, you will end up spending less in the long run! <br />
-
-        To shorten things up, if you have good input, you will get a fantastic output!
-        If you have a bad input, then you will have a horrible output
-        
-    </p>
+      <p>Healthy eating is fundamental for getting into shape. This one aspect is a very difficult task to accomplish, because not only do you have to watch what you eat, but also 
+        have the discipline to go through the day and make healthy choices. 
+      </p>
+      <p>When a person says to "watch what you eat" they are not ony saying make healthier choices, but to also watch how much food you consume throughout the day. Each person has a different
+        consumption rate based on different factors such as weight, age, sex and in some cases the type of fitness goals you are working towards (weight loss, building muscle, physic fitness etc.). Talking
+        to a nutritionist or medical professional is highly recommended, but if a nutritionist is something you don't want to see at the moment here are some tips that might he helpful when starting your health journey: </p>
+        <ol>
+          <li>Consume more water - Drinking water can increase chances of weight loss and promote weight maintenance. Drinking water may also reduce a person appetite, which can lead to less food intake. Other beverages such as 
+            soda and beer tend to add pressure to your stomach, giving you that bloated feeling while water does the exact opposite.
+          </li>
+          <li>Stick with a list while shopping- When shopping stick to your grocery list! Never enter a store hungry, this keeps impulsive buying at bay.</li>
+          <li>Start Meal prepping- Meal prepping will make your life easier, it also increases the chances of success by making healthier choices and controlling your proportions. </li>
+        </ol>
+        <p>Eating healthy is a subject that could be talked about for days. There is a plethora of information and helpful tips to learn about this subject. Click on the fridge handle to learn more about types of foods that can help you
+          on your health journey and how eating these foods and help your body during your health journey. </p>
       <div>
         <button className="btn-left" onClick ={() => navigate(-1)}>Previous</button>
         <NavLink to="/">
