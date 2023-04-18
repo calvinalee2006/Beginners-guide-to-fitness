4c12adfb1d0c53d401f34452936d76950030ae83
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-04-18 06:15:37 -0500

    import index.html to olve issue?

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import '.index.html'
 import './index.css'
 import "./styles.css"
 import "slick-carousel/slick/slick.css";
@@ -8,7 +9,7 @@ import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <index.html />,
+<index.html />,
 <App />)
 
 
