8014b1e27fb738b2b511b35a53d1cec22a78fd3b
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-04-18 06:18:42 -0500

    try again!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import '.index.html'
 import './index.css'
 import "./styles.css"
 import "slick-carousel/slick/slick.css";
@@ -9,7 +8,6 @@ import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<index.html />,
 <App />)
 
 
