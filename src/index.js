6acf17fbfbfc50a318fa55b379f1718dd484a5e5
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-04-20 06:46:57 -0500

    Used Hash router

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css'
 import "./styles.css"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { HashRouter } from "react-router-dom";
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+    <HashRouter>
+    document.getElementById('root'));
 root.render(
-<App />,
+<App />
+</HashRouter>
 )
 
 
