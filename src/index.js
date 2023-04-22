be79463fe8eeab0dd88c2871dd9d2df681f92863
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-04-21 19:03:36 -0500

    updated Scripts in package.json

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ import './index.css'
 import "./styles.css"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { HashRouter } from "react-router-dom";
 
 
-const root = ReactDOM.createRoot(
-    <HashRouter>
-<App />
-</HashRouter>,    
-document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <App />  
 )
+  
+
 
 
 
